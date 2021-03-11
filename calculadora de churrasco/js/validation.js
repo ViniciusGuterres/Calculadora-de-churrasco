767576215294fafab3fbf9f4901c22a5c4457a59
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2021-03-11 15:18:33 -0300

    Delete calculadora de churrasco directory

--- calculadora de churrasco/js/validation.js
+++
@@ -1,8 +0,0 @@
-// coleting the inputs values
-const homens = document.getElementById("homens-qnt");
-const mulheres = document.getElementById("mulhers-qnt");
-const criancas = document.getElementById("criancas-qnt");
-
-const bebemAlcool = document.getElementById("bebidas-alcoolicas");
-const naoBebemAlcool = document.getElementById("bebidas-nao-alcoolicas");
-
